2f43a8bf06b703ed309be52bec310e8d2450ed42
Touha Zohair <touha98@gmail.com>
2023-05-12 12:39:32 +0600

    update admin header

--- topten/urls.py
+++ topten/urls.py
@@ -2,6 +2,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
+admin.site.site_header = 'Top Ten Agro Ltd. administration'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
